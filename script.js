38cf2c7088436bfcc4b5003a6e3c459d3f6cb47f
aditya raj <adityaraj25042005@gmail.com>
2026-05-30 15:51:13 +0530

    Connected Render backend

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ async function submitAdmission() {
     try {
 
         const response = await fetch(
-            "http://localhost:5000/admission",
+            "https://aim-point.onrender.com/admission",
             {
                 method: "POST",
                 headers: {
